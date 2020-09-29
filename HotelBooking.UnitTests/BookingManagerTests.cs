ca259b863f3fe47290f3ce89cb91d831415b8e1d
SamPerCap <31726611+SamPerCap@users.noreply.github.com>
2020-09-29 18:24:21 +0200

    Back to normal

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -86,7 +86,7 @@ namespace HotelBooking.UnitTests
             };
             var result = bController.Post(booking);
 
-            mockBookingRepository.Verify(x => x.Add(booking), Times.Never);
+            mockBookingRepository.Verify(x => x.Add(booking), Times.Once);
         }
         [Fact]
         public void CreateBooking_StartAndEndAreInsideBookedPeriod_ReturnFalse()
